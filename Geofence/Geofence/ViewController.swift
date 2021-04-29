5455d97f4cc8bbd6d85369490563d4acf608ff8a
Thongchai Subsaidee <chanoktrue@me.com>
2021-04-29 08:52:53 +0700

    first project

---
+++ Geofence/Geofence/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Geofence
+//
+//  Created by Thongchai Subsaidee on 29/4/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
